e6f18897f5578619201dacf436d824f18727e13f
Vaibhav Patil <6760119+vaibhavpatilai@users.noreply.github.com>
2020-03-17 13:42:52 -0400

    Added load balanced rest template
    Added load balanced rest template

--- api-gateway/src/main/java/com/pharmachain/gateway/config/ApplicationConfiguration.java
+++ api-gateway/src/main/java/com/pharmachain/gateway/config/ApplicationConfiguration.java
@@ -1,7 +1,9 @@
 package com.pharmachain.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import com.pharmachain.gateway.filter.PreFilter;
 
@@ -11,5 +13,11 @@ public class ApplicationConfiguration {
 	@Bean
 	public PreFilter preFilter() {
 		return new PreFilter();
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
