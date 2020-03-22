7651914ee3b2ab2a7bf478b8dbaa19bae38ff44a
Vaibhav Patil <6760119+vaibhavpatilai@users.noreply.github.com>
2020-03-22 11:56:58 -0400

    Order History flow completed
    Order History flow completed

--- order-webapp/src/app/orders/order-history/order-history.component.ts
+++ order-webapp/src/app/orders/order-history/order-history.component.ts
@@ -3,6 +3,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Order } from '../order.model';
+import { TokenService } from 'src/app/shared/services/token.service';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-order-history',
@@ -11,15 +14,22 @@ import { map } from 'rxjs/operators';
 })
 export class OrderHistoryComponent implements OnInit {
 
+  orders: Order[];
+
   isHandset: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
     .pipe(
       map(result => result.matches)
     );
 
   constructor(private breakpointObserver: BreakpointObserver,
-              private router: Router) { }
+              private router: Router,
+              private tokenService: TokenService,
+              private orderService: OrderService) { }
 
   ngOnInit() {
+    this.orderService.getOrderHistory(this.tokenService.getCustomerId()).subscribe((orders: Order[]) => {
+      this.orders = orders;
+    });
   }
 
   onCreateOrder() {
--- order-webapp/src/app/orders/order.service.ts
+++ order-webapp/src/app/orders/order.service.ts
@@ -3,16 +3,21 @@ import { Store } from './store.model';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { Order } from './order.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  baseAPIUrl = environment.baseAPIUrl + '/stores';
+  baseAPIUrl = environment.baseAPIUrl + '/orders';
   storesUpdated = new Subject<Store[]>();
 
   constructor(private httpClient: HttpClient) { }
+
+  public getOrderHistory(customerId: string) {
+    return this.httpClient.get<Order[]>(this.baseAPIUrl + '/history' + `/${customerId}`);
+  }
 
   public getStore(storeId: string) {
     return this.httpClient.get<Store>(this.baseAPIUrl + `/${storeId}`);
