120dc909d2993a20fa8f35c93f77d234ccf895bb
Vaibhav Patil <6760119+vaibhavpatilai@users.noreply.github.com>
2020-03-25 11:37:35 -0400

    Set Timeout reason
    Set Timeout reason

--- order-webapp/src/app/shared/directives/autofocus.directive.ts
+++ order-webapp/src/app/shared/directives/autofocus.directive.ts
@@ -8,6 +8,7 @@ export class AutofocusDirective implements OnInit {
   constructor(public elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    // Use setTimeout to queue the current task, this task will be executed after current execution is done
     setTimeout(() => {
       (this.elementRef.nativeElement as any).focus.apply(this.elementRef.nativeElement);
     });
